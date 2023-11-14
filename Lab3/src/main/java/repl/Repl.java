9045a1a93f3771c1514efbd06c3753742168ffef
Katrincik <grebennicova.ecaterina@gmail.com>
2023-11-14 04:12:13 +0200

    Made the options to be displayed every time

--- Lab3/src/main/java/repl/Repl.java
+++ Lab3/src/main/java/repl/Repl.java
@@ -8,29 +8,38 @@ import java.util.Scanner;
  * Read Eval Print Loop.
  */
 public class Repl {
-  public void run(Snapshot snapshot) {
-    while (true) {
-      try {
+    public void run(Snapshot snapshot) {
         Scanner input = new Scanner(System.in);
-        String choiceInput = input.nextLine();
-        String[] args = choiceInput.split("\\s+");
-        System.out.println("The input is:" + choiceInput);
-        CommandFactory factory = new CommandFactory();
-        Command command = factory.build(args[0]);
-        command.execute(args, snapshot);
-        //read input
-        //evoluate it
-        //print it that did user
-        //continue cause it is like a loop
-        // TODO: get user input.
 
-        // TODO: create the appropriate command via 'CommandFactory'.
+        while (true) {
+            try {
+                // Display options
+                System.out.println("What do you want to do?");
+                System.out.println("commit - For updating the snapshot");
+                System.out.println("info <filename> - For printing general info");
+                System.out.println("status - For checking files changes");
 
-        // TODO: Run the command with all the user args.
-      } catch (Exception e) {
-        System.out.println(e.toString());
-        // TODO: decide what to do in case an exception occurs.
-      }
+                // Read user input
+                String choiceInput = input.nextLine();
+                String[] args = choiceInput.split("\\s+");
+                System.out.println();
+                System.out.println("You applied for - " + choiceInput);
+
+                // Create and execute command
+                CommandFactory factory = new CommandFactory();
+                Command command = factory.build(args[0]);
+                command.execute(args, snapshot);
+                // TODO: get user input.
+
+                // TODO: create the appropriate command via 'CommandFactory'.
+
+                // TODO: Run the command with all the user args.
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error: " + e.getMessage());
+            } catch (Exception e) {
+                System.out.println("An unexpected error occurred: " + e.toString());
+                // TODO: decide what to do in case an exception occurs.
+            }
+        }
     }
-  }
 }
