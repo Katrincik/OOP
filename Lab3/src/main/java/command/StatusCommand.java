806686614e8266be3d44206a1dc8903592d65307
Katrincik <grebennicova.ecaterina@gmail.com>
2023-11-14 04:55:58 +0200

    Implemented File add/delete detection output

--- Lab3/src/main/java/command/StatusCommand.java
+++ Lab3/src/main/java/command/StatusCommand.java
@@ -6,6 +6,8 @@ import Lab3.src.main.java.snapshot.Snapshot;
 import java.io.IOException;
 import java.nio.file.*;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 public class StatusCommand implements Command {
   @Override
@@ -17,33 +19,45 @@ public class StatusCommand implements Command {
 
     System.out.println("Last Snapshot Time: " + LocalDateTime.now());
 
-    // Flag to track if any files have changed since the last snapshot
-    boolean filesChanged = false;
+    Set<String> currentFiles = new HashSet<>();
+    Set<String> lastSnapshotFiles = new HashSet<>(snapshot.getLastSnapshotFiles());
+
     String folderPath = "Lab3/src/main/java/data/";
 
     try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(folderPath))) {
       for (Path file : stream) {
         if (Files.isRegularFile(file)) {
-          // Retrieves the last modified time of the file
-          long fileLastModifiedTime = Files.getLastModifiedTime(file).toMillis();
           String relativePath = file.getFileName().toString();
-          // Checks if the file was modified after the last snapshot
-          if (fileLastModifiedTime > snapshot.getLastSnapshotTime()) {
-            System.out.println(relativePath + " - Changed ");
-            filesChanged = true;
-          } else {
-              System.out.println(relativePath + " - No Change");
+          currentFiles.add(relativePath);
+
+          // Check for new files
+          if (!lastSnapshotFiles.contains(relativePath)) {
+            System.out.println(relativePath + " - New File");
+            continue;
           }
+
+          // Check for changed files
+          long fileLastModifiedTime = Files.getLastModifiedTime(file).toMillis();
+          if (fileLastModifiedTime > snapshot.getLastSnapshotTime()) {
+            System.out.println(relativePath + " - Changed");
+          } else {
+            System.out.println(relativePath + " - No Change");
+          }
+
+          // Remove checked file from the set
+          lastSnapshotFiles.remove(relativePath);
         }
       }
     } catch (IOException e) {
       e.printStackTrace();
     }
 
-    if (!filesChanged) {
-      System.out.println("No files were changed since the last snapshot.\n");
-    } else {
-      System.out.println("Some files were changed since the last snapshot.\n");
+    // Detect deleted files
+    for (String file : lastSnapshotFiles) {
+      System.out.println(file + " - Deleted");
     }
+
+    // Update the snapshot with the current state of files
+    snapshot.setLastSnapshotFiles(currentFiles);
   }
 }
--- Lab3/src/main/java/snapshot/Snapshot.java
+++ Lab3/src/main/java/snapshot/Snapshot.java
@@ -2,7 +2,9 @@ package Lab3.src.main.java.snapshot;
 
 import java.io.*;
 import java.nio.file.*;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Snapshot {
     // TODO: add necessary attributes storing the state about all of the files. It
@@ -12,17 +14,23 @@ public class Snapshot {
     // The timestamp of the last snapshot
     private long lastSnapshotTime;
 
+    private Set<String> lastSnapshotFiles;
+
+    // The file path for storing the snapshot files
+    private final String filesFileName = "Lab3/src/main/java/snapshot/snapshot_files.txt";
+
     // The file path for storing the snapshot timestamp
-    private final String fileName = "Lab3/src/main/java/snapshot/snapshot_time.txt";
+    private final String timeFileName = "Lab3/src/main/java/snapshot/snapshot_time.txt";
 
     public Snapshot() {
         this.lastSnapshotTime = loadLastSnapshotTime();
+        this.lastSnapshotFiles = loadLastSnapshotFiles();
     }
 
     // If the file exists and is not empty, it reads the first line as the timestamp.
     // If the file doesn't exist or an error occurs, it sets the current time as the last snapshot time.
     private long loadLastSnapshotTime() {
-        Path path = FileSystems.getDefault().getPath(fileName);
+        Path path = FileSystems.getDefault().getPath(timeFileName);
         if (Files.exists(path)) {
             try {
                 List<String> lines = Files.readAllLines(path);
@@ -36,10 +44,29 @@ public class Snapshot {
         return System.currentTimeMillis();
     }
 
+    private Set<String> loadLastSnapshotFiles() {
+        Path path = FileSystems.getDefault().getPath(filesFileName);
+        Set<String> files = new HashSet<>();
+        if (Files.exists(path)) {
+            try {
+                List<String> lines = Files.readAllLines(path);
+                files.addAll(lines);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return files;
+    }
+
     // Saves the current time as the last snapshot time in a file
     public void saveLastSnapshotTime() {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-            writer.write(Long.toString(System.currentTimeMillis()));
+        try (BufferedWriter timeWriter = new BufferedWriter(new FileWriter(timeFileName));
+             BufferedWriter filesWriter = new BufferedWriter(new FileWriter(filesFileName))) {
+            timeWriter.write(Long.toString(System.currentTimeMillis()));
+            for (String file : lastSnapshotFiles) {
+                filesWriter.write(file);
+                filesWriter.newLine();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -47,5 +74,13 @@ public class Snapshot {
 
     public long getLastSnapshotTime() {
         return lastSnapshotTime;
+    }
+
+    public Set<String> getLastSnapshotFiles() {
+        return lastSnapshotFiles;
+    }
+
+    public void setLastSnapshotFiles(Set<String> files) {
+        this.lastSnapshotFiles = files;
     }
 }
