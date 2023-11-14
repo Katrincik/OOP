ded03e65d3144901c58411177d1c56a78a02b8c9
Katrincik <grebennicova.ecaterina@gmail.com>
2023-11-14 14:00:31 +0200

    Added some outputs to check the last snapshot time and files

--- Lab3/src/main/java/command/CommitCommand.java
+++ Lab3/src/main/java/command/CommitCommand.java
@@ -3,12 +3,18 @@ package Lab3.src.main.java.command;
 import Lab3.src.main.java.command.Command;
 import Lab3.src.main.java.snapshot.Snapshot;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 public class CommitCommand implements Command {
   public void execute(String[] args, Snapshot snapshot) {
     // TODO: implement.
     System.out.println("Execute commit");
 
     snapshot.saveLastSnapshotTime();
+    System.out.println("Last snapshot time: " + LocalDateTime.ofInstant(Instant.ofEpochMilli(snapshot.getLastSnapshotTime()), ZoneId.systemDefault()));
+    System.out.println("Last snapshot files: " + snapshot.getLastSnapshotFiles());
     System.out.println("Snapshot time updated. Current state is now the baseline for future changes.\n");
   }
 }
